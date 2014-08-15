4ff8d8ed421934a03c6d4cb144c0fdd2e350463a
CCDM04 <LABORATORIOMAC@iMac-de-CCDM04.local>
2014-08-15 10:08:46 -0600

    Initial Commit

---
+++ ejercicioUno/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ejercicioUno
+//
+//  Created by CCDM04 on 15/08/14.
+//  Copyright (c) 2014 CCDM04. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+

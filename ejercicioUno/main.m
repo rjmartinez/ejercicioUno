e9804eb768a7585f8d305089a764aca22ca1dfc9
CCDM04 <LABORATORIOMAC@iMac-de-CCDM04.local>
2014-08-15 11:25:51 -0600

    Commit inicial

--- ejercicioUno/main.m
+++ ejercicioUno/main.m
@@ -11,10 +11,81 @@
 int main(int argc, const char * argv[])
 {
 
-    @autoreleasepool {
+    @autoreleasepool
+    {
+        NSString *mensaje = @"Hola a la calculadora";
+        NSLog(@"%@",mensaje);
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        float primerValor;
+        scanf("%f",&primerValor);
+        
+        int opcion;
+        scanf("%d",&opcion);
+        
+        float segundoValor;
+        scanf("%f",&segundoValor);
+        
+        float operacion=0;
+        
+        switch (opcion) {
+            case 0:
+                operacion = primerValor + segundoValor;
+                break;
+            case 1:
+                operacion = primerValor - segundoValor;
+                break;
+            case 2:
+                operacion = primerValor * segundoValor;
+                break;
+            case 3:
+                operacion = primerValor / segundoValor;
+                break;
+            default:
+                NSLog(@"Opción no válida.");
+                break;
+        }
+        
+        if(operacion > 100)
+        {
+            NSLog(@"mensaje 1");
+        }
+        else
+        {
+            NSLog(@"mensaje 2");
+        }
+        
+        NSMutableArray *arrayOperaciones = [[NSMutableArray alloc] init];
+        
+        NSMutableArray *array;
+        array = [NSMutableArray alloc];
+        array = [NSMutableArray init];
+        
+        NSNumber *numero;
+        numero = [NSNumber alloc];
+        numero = [[NSNumber alloc] initWithFloat:operacion];
+        
+        
+        [array addObject:numero];
+        [array count];
+        
+        NSDictionary *dictionary = @{
+                                     @"nombreç": @"Javier",
+                                     @"edad":@"100",
+                                     @"lenguajes":@"Objective-C"
+                                     };
+        
+        NSMutableDictionary * dic = [[NSMutableDictionary alloc]init];
+        [dic setValue:@"Martinez" forKey:@"apellido"];
+        
+        [arrayOperaciones addObject:dic];
+        [arrayOperaciones addObject:dictionary];
+        
+        
+        id objeto;
+        for (array in objeto)
+        {
+            NSLog(@"%@",objeto);
+        }
         
     }
     return 0;
